d0f1682aeaa5d3c84996659dae936173444c36cc
FlutLab.io <>
2022-10-07 17:22:20 +0000

    20221007-2

---
+++ lib/all_imports.dart
@@ -0,0 +1,11 @@
+export 'package:flutter/material.dart';
+export 'package:flutter_riverpod/flutter_riverpod.dart';
+export 'package:flex_color_scheme/flex_color_scheme.dart';
+export 'package:riverpoduniversity/themes/flexcolorscheme.dart';
+export 'package:google_fonts/google_fonts.dart';
+
+export 'package:riverpoduniversity/providers/theme_provider.dart';
+export 'package:riverpoduniversity/providers/counter_provider.dart';
+
+export 'package:riverpoduniversity/widgets/appbar_widget.dart';
+export 'package:riverpoduniversity/widgets/drawer_widget.dart';
---
+++ lib/widgets/appbar_widget.dart
@@ -0,0 +1,21 @@
+import 'package:riverpoduniversity/all_imports.dart';
+
+PreferredSizeWidget appbarWidget = AppBar(
+  title: const Text('Riverpod University'),
+  centerTitle: true,
+  actions: [
+    Consumer(
+      builder: (context, ref, child) {
+        return IconButton(
+          onPressed: () {
+            ref.invalidate(counterProvider);
+            ref.invalidate(themeProvider);
+          },
+          icon: const Icon(
+            Icons.restart_alt,
+          ),
+        );
+      },
+    ),
+  ],
+);
