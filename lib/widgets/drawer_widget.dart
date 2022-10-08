37781fcdd6d8b315689aab8537f1c69082bdf2cf
FlutLab.io <>
2022-10-08 17:31:25 +0000

    Switching to branch 20221008-1

--- lib/widgets/drawer_widget.dart
+++ lib/widgets/drawer_widget.dart
@@ -3,13 +3,36 @@ import 'package:riverpoduniversity/all_imports.dart';
 Widget drawerWidget = Drawer(
   child: Column(
     children: [
-      Image.asset('assets/RiverpodUniversityLogo.png'),
+      Image.asset('assets/riverpod_university_logo.png'),
       ListTile(
-        title: const Text('Light me up!'),
+        title: const Text('Yin Yang switcher'),
+        //Add Consumer() to get acces to refs, it wants a
+        //builder: (BuildContext, WidgetRef, Widget?)
+        trailing: (Consumer(
+          builder: (context, ref, child) {
+            return Switch(
+                //Watch the bool from theme_provider.dart
+                value: ref.watch(themeProvider),
+                //Pass value to callback
+                onChanged: (value) {
+                  if (value == true) {
+                    ref.read(themeProvider.notifier).setLightTheme();
+                  } else {
+                    ref.read(themeProvider.notifier).setDarkTheme();
+                  }
+                });
+          },
+        )),
+      ),
+      ListTile(
+        title: const Text('Go Yang'),
+        //Add Consumer() to get acces to refs, it wants a
+        //builder: (BuildContext, WidgetRef, Widget?)
         trailing: Consumer(
           builder: ((context, ref, child) {
             return IconButton(
               onPressed: () {
+                //Use the function from theme_provider.dart
                 ref.read(themeProvider.notifier).setLightTheme();
               },
               icon: const Icon(Icons.light_mode),
@@ -19,10 +42,13 @@ Widget drawerWidget = Drawer(
         ),
       ),
       ListTile(
-        title: const Text('Go to the Dark Side!'),
+        title: const Text('Go Yin'),
+        //Add Consumer() to get acces to refs, it wants a
+        //builder: (BuildContext, WidgetRef, Widget?)
         trailing: Consumer(builder: (context, ref, child) {
           return IconButton(
             onPressed: () {
+              //Use the function from theme_provider.dart
               ref.read(themeProvider.notifier).setDarkTheme();
             },
             icon: const Icon(Icons.dark_mode),
