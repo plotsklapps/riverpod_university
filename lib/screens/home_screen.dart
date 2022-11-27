754ad306ff1576e6862697ac61a90c4cab7e21a5
plotsklapps <plotsklapps@gmail.com>
2022-11-27 09:39:57 +0000

    Extracted NavButton() to separate page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,50 +4,30 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appbarWidget,
-      drawer: drawerWidget,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const NavButton(
-              buttonText: 'Counter',
-              destination: 'counter_screen',
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            const NavButton(
-              buttonText: 'Weather',
-              destination: 'weather_screen',
-            ),
-          ],
+    //SafeArea FTW. Makes the app evade notches and stuff
+    return SafeArea(
+      child: Scaffold(
+        appBar: appbarWidget,
+        drawer: drawerWidget,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              //NavButton to be found in navbutton_widget.dart
+              NavButton(
+                buttonText: 'Counter',
+                destination: 'counter_screen',
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              NavButton(
+                buttonText: 'Weather',
+                destination: 'weather_screen',
+              ),
+            ],
+          ),
         ),
-      ),
-    );
-  }
-}
-
-class NavButton extends StatelessWidget {
-  const NavButton(
-      {Key? key, required this.buttonText, required this.destination})
-      : super(key: key);
-
-  final String buttonText;
-  final String destination;
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        Navigator.pushNamed(
-          context,
-          destination,
-        );
-      },
-      child: Text(
-        buttonText,
       ),
     );
   }
